0f7deedbce4dd589770ab6a56a860240db8967f6
peinwastaken <legokid173@gmail.com>
2025-12-25 01:09:30 +0200

    start server mod

---
+++ BorkelRNVGServer/BorkelRNVG.cs
@@ -0,0 +1,18 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Logging;
+using SPTarkov.Server.Core.Models.Utils;
+
+namespace BorkelRNVGServer
+{
+    [Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
+    public class BorkelRNVG(ISptLogger<BorkelRNVG> logger) : IOnLoad
+    {
+        public Task OnLoad()
+        {
+            logger.LogWithColor("Successfully loaded Borkel's realistic NVGs!", LogTextColor.Green);
+            
+            return Task.CompletedTask;   
+        }
+    }
+}
---
+++ BorkelRNVGServer/Metadata.cs
@@ -0,0 +1,22 @@
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using Range = SemanticVersioning.Range;
+using Version = SemanticVersioning.Version;
+
+namespace BorkelRNVGServer
+{
+    public record Metadata : AbstractModMetadata
+    {
+        public override string ModGuid { get; init; } = "com.borkel.nvgmasks";
+        public override string Name { get; init; } =  "Borkel's Realistic Night Vision Goggles";
+        public override string Author { get; init; } = "Borkel";
+        public override List<string>? Contributors { get; init; } = ["Fontaine", "Mirni", "CJ", "GrooveypenguinX", "Choccster", "kiobu-kouhai", "DrakiaXYZ", "kiki", "Props", "Mattdokn"];
+        public override Version Version { get; init; } = new Version("2.0.0");
+        public override Range SptVersion { get; init; } = new Range("~4.0.0");
+        public override string? Url { get; init; } = "https://github.com/Borkel/RealisticNVG-client-2/";
+        public override bool? IsBundleMod { get; init; } = true;
+        public override string License { get; init; } = "Creative Commons BY-NC-SA 3.0";
+        
+        public override List<string>? Incompatibilities { get; init; }
+        public override Dictionary<string, Range>? ModDependencies { get; init; }
+    }
+}
