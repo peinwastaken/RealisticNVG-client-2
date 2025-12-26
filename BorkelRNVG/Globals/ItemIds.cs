fff200d9614cdff6a5638b44f8e70caa59549a04
peinwastaken <legokid173@gmail.com>
2025-12-26 11:13:02 +0200

    pvs-14 id fix

--- BorkelRNVG/Globals/ItemIds.cs
+++ BorkelRNVG/Globals/ItemIds.cs
@@ -1,10 +1,12 @@
 namespace BorkelRNVG.Globals
 {
-    public class ItemIds
+    public static class ItemIds
     {
         public static readonly string GPNVG = "5c0558060db834001b735271";
-        public static readonly string PVS14 = "5c110624d174af029e69734c";
+        public static readonly string PVS14 = "57235b6f24597759bf5a30f1";
         public static readonly string N15 = "5c066e3a0db834001b7353f0";
-        public static readonly string T7 = "5c110624d174af029e69734c";
+        public static readonly string T7 = "5c110624d174af029e69734c"
+        public static readonly string PNV10 = "5c0696830db834001d23f5da"
+        public static readonly string PNV57 = "67506ca81f18589016006aa6";
     }
 }
